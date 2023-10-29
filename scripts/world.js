b74c1b2948bc08d3919bb676c04705a43234abbd
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-Job.local>
2023-10-29 20:35:00 +0100

    simplex noise

--- scripts/world.js
+++ scripts/world.js
@@ -1,36 +1,31 @@
-import * as THREE from "three";
-
+import * as THREE from 'three';
+import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js';
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
-  /**
-   * @type {{
-   * id: number,
-   * instanceId: number
-   * }[][][]}
-   */
-
   data = [];
+
+  params = {
+    terrain: {
+      scale: 30,
+      magnitude: 0.5,
+      offset: 0.2,
+    },
+  };
 
   constructor(size = { width: 64, height: 32 }) {
     super();
     this.size = size;
   }
 
-  /**
-   * Generate the world data and meshes
-   */
-
   generate() {
-    this.generateTerrain();
+    this.initializeTerrain();
     this.generateMeshes();
   }
-  /**
-   * Generate the world terrain data
-   */
-  generateTerrain() {
+
+  initializeTerrain() {
     this.data = [];
     for (let x = 0; x < this.size.width; x++) {
       const slice = [];
@@ -38,7 +33,7 @@ export class World extends THREE.Group {
         const row = [];
         for (let z = 0; z < this.size.width; z++) {
           row.push({
-            id: 1,
+            id: 0,
             instanceId: null,
           });
         }
@@ -48,11 +43,26 @@ export class World extends THREE.Group {
     }
   }
 
-  /**
-   * Generate the 3d representation of the world from the world data
-   */
   generateMeshes() {
     this.clear();
+
+    const simplex = new SimplexNoise();
+    for (let x = 0; x < this.size.width; x++) {
+      for (let z = 0; z < this.size.width; z++) {
+        const value = simplex.noise(
+          x / this.params.terrain.scale,
+          z / this.params.terrain.scale
+        );
+        const scaledNoise =
+          this.params.terrain.offset + this.params.terrain.magnitude * value;
+        let height = Math.floor(this.size.height * scaledNoise);
+        height = Math.max(0, Math.min(height, this.size.height - 1));
+
+        for (let y = 0; y <= height; y++) {
+          this.setBlockId(x, y, z, 1);
+        }
+      }
+    }
 
     const maxCount = this.size.width * this.size.width * this.size.height;
     const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
@@ -70,20 +80,14 @@ export class World extends THREE.Group {
             mesh.setMatrixAt(instanceId, matrix);
             this.setBlockInstanceId(x, y, z, instanceId);
             mesh.count++;
-          } // Added closing bracket here
+          }
         }
       }
     }
+
     this.add(mesh);
   }
 
-  /**
-   * Gets the block data at (x, y, z)
-   * @param {number} x
-   * @param {number} y
-   * @param {number} z
-   * @returns {{id: number, instanceId: number}}
-   */
   getBlock(x, y, z) {
     if (this.inBounds(x, y, z)) {
       return this.data[x][y][z];
@@ -92,39 +96,18 @@ export class World extends THREE.Group {
     }
   }
 
-  /**
-   * Sets the block id for the block at {x, y, z}
-   * @param {number} x
-   * @param {number} y
-   * @param {number} z
-   * @param {number} id
-   */
   setBlockId(x, y, z, id) {
     if (this.inBounds(x, y, z)) {
       this.data[x][y][z].id = id;
     }
   }
 
-  /**
-   * Sets the block instance id for the block at {x, y, z}
-   * @param {number} x
-   * @param {number} y
-   * @param {number} z
-   * @param {number} instanceId
-   */
-
   setBlockInstanceId(x, y, z, instanceId) {
     if (this.inBounds(x, y, z)) {
       this.data[x][y][z].instanceId = instanceId;
     }
   }
-  /**
-   * Checks if the (x, y, z) coordinates are within bounds
-   * @param {number} x
-   * @param {number} y
-   * @param {number} z
-   * @returns {boolean}
-   */
+
   inBounds(x, y, z) {
     if (
       x >= 0 &&
