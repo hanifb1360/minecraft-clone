c98871abb9ad82a752ecdbba308cd1b5fddd0527
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-Job.local>
2023-10-29 15:16:28 +0100

    seperated world data from 3d representaion of world

--- scripts/ui.js
+++ scripts/ui.js
@@ -4,5 +4,8 @@ export function createUI(world) {
     const gui = new GUI();
     gui.add(world.size, 'width', 8, 128, 1).name('width');
     gui.add(world.size, 'height', 8, 64, 1).name('height');
-    gui.add(world, 'generate'); 
+    
+    gui.onChange(() => {
+        world.generate();
+    });
 }
