fbac3c8bd1b38c3aee6c5e18b9a020bd00947fd1
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-Job.local>
2023-10-29 11:19:34 +0100

    extended y dimention of the world

--- scripts/world.js
+++ scripts/world.js
@@ -4,18 +4,20 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
-    constructor(size = 32) {
+    constructor(size = { width: 64, height: 32}) {
         super();
         this.size = size;
     }
 
     generate() {
-        for (let x = 0; x < this.size; x++) {
-            for (let z = 0; z < this.size; z++) {
+        for (let x = 0; x < this.size.width; x++) {
+            for (let y = 0; y < this.size.height; y++) {
+            for (let z = 0; z < this.size.width; z++) {
               const block = new THREE.Mesh(geometry, material);
-              block.position.set(x, 0, z);
+              block.position.set(x, y, z);
               this.add(block);
             }
+        }
           } 
     }
 } 
