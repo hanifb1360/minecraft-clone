3e7acb2d2b3b29ecfde1f2e4f5a940b50c66a159
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-Job.local>
2023-10-29 10:37:38 +0100

    expanded ground-basic

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Renderer setup
 
@@ -9,7 +9,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Camera setup
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
@@ -19,37 +24,45 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
 function setupLights() {
-    const light1 = new THREE.DirectionalLight();
-    light1.position.set(1, 1, 1);
-    scene.add(light1);
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
 
-    const light2 = new THREE.DirectionalLight();
-    light2.position.set(-1, 1, -0.5);
-    scene.add(light2);
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, 1, -0.5);
+  scene.add(light2);
 
-    const ambient = new THREE.AmbientLight();
-    ambient.intensity = 0.1;
-    scene.add(ambient);
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
+}
+
+function setupWorld(size) {
+  for (let x = 0; x < size; x++) {
+    for (let z = 0; z < size; z++) {
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.set(x, 0, z);
+      scene.add(cube);
+    }
+  }
 }
 
 // Render loop
 function animate() {
-    requestAnimationFrame(animate);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-
 setupLights();
+setupWorld(32);
 animate();
