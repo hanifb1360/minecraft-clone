0d2e8857959445e446175e86fe3dd6dedcf8da61
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-Job.local>
2023-10-29 11:08:33 +0100

    refactored world from main js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { World } from "./world";
 
 // Renderer setup
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x80a0e0);
 document.body.appendChild(renderer.domElement);
 
 // Camera setup
@@ -15,15 +17,19 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
+camera.position.set(-32, 16, -32);
+
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(16, 0, 16);
+controls.update();
 
 // Scene setup
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
+const world = new World();
+
+world.generate();
+scene.add(world);
 
 function setupLights() {
   const light1 = new THREE.DirectionalLight();
@@ -39,15 +45,7 @@ function setupLights() {
   scene.add(ambient);
 }
 
-function setupWorld(size) {
-  for (let x = 0; x < size; x++) {
-    for (let z = 0; z < size; z++) {
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position.set(x, 0, z);
-      scene.add(cube);
-    }
-  }
-}
+
 
 // Render loop
 function animate() {
@@ -64,5 +62,4 @@ window.addEventListener("resize", () => {
 });
 
 setupLights();
-setupWorld(32);
 animate();
